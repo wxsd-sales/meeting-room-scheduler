d207d6652fa7e7d9b5babe1327ec3044d63e48ea
Taylor Hanson <tahanson@cisco.com>
2026-03-03 11:13:23 -0500

    macro updated

--- deviceMacro.js
+++ deviceMacro.js
@@ -1,7 +1,12 @@
 import xapi from 'xapi';
 
-//This will most likely match the BASE_URL in your server's .env file
-const baseUrl = "";
+const connectUrl = "";
+
+var serialNumber;
+xapi.Status.SystemUnit.Hardware.Module.SerialNumber.get().then(value => {
+  console.log("Serial Number:", value);
+  serialNumber = value;
+})
 
 // Enable the HTTP client if it isn't already
 xapi.Config.HttpClient.Mode.get().then(value => {
@@ -12,14 +17,6 @@ xapi.Config.HttpClient.Mode.get().then(value => {
   }
 });
 
-// You can remove this section if your webserver is secure.
-xapi.Config.HttpClient.AllowInsecureHTTPS.get().then(value => {
-  console.log('AllowInsecureHTTPS : ' + value);
-  if(value == 'False'){
-    console.log('Enabling AllowInsecureHTTPS');
-    xapi.Config.HttpClient.AllowInsecureHTTPS.set('True');
-  }
-});
 
 // Add the Button to the touch panel
 xapi.command('UserInterface Extensions Panel Save', {
@@ -50,52 +47,33 @@ xapi.event.on('UserInterface Extensions Panel Clicked', (event) => {
         Placeholder: ' ',
         Duration: 0,
       }).catch((error) => { console.error(error); });
-
     }
 });
 
-// Handle all the SMS Invite preparation screens
 xapi.event.on('UserInterface Message TextInput Response', (event) => {
   switch(event.FeedbackId){
     case 'enter_code':
-      dialMeeting(event.Text);
+      requestDial(event.Text);
   }
 });
 
 
-function dialMeeting(code){
-
+function requestDial(code){
   console.log('Code: ' + code);
-  let url = `${baseUrl}/sip/${code}`;
-  console.log('Url:', url);
-  xapi.command('HttpClient Get', { 
+  let payload = {
+    "deviceSerial":serialNumber,
+    "hostCode":code
+  }
+  xapi.command('HttpClient Post', { 
     Header: ["Content-Type: application/json"], 
-    Url: url,
+    Url: connectUrl,
     ResultBody: 'plaintext'
-  }).then((result) => {
+  }, JSON.stringify(payload)).then((result) => {
+    console.log("WebexConnect Response:");
     console.log(result.Body);
-    var body = JSON.parse(result.Body)
-    if(body.sipAddress){
-      if(body.minutesEarly > 10){
-        xapi.Command.UserInterface.Message.Alert.Display
-          ({ Duration: 3,
-             Text: 'Meeting starts in more than 10 minutes',
-             Title: 'Error'});
-      } else {
-        console.log("Dialing...");
-        xapi.Command.Dial({Number:body.sipAddress, Protocol:"SIP"});
-      }
-
-    } else {
-      xapi.Command.UserInterface.Message.Alert.Display
-          ({ Duration: 3,
-             Text: 'Meeting not found',
-             Title: 'Error'});
-    }
   }).catch((err) => {
-    console.log("Error: ");
-    console.log(err);
-        
+    console.error("requestDial Error: ");
+    console.error(err);
     // Should close panel and display errors
     xapi.Command.UserInterface.Message.Alert.Display
         ({ Duration: 3,
